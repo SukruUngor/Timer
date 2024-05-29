152d9ff89a8d1c79b6a283e9af92d53330009d2f
Sukru Ungor <sukruungor@gmail.com>
2024-05-29 19:35:50 +0300

    Timer

---
+++ sukru1/sukru1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace sukru1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.baslat = new System.Windows.Forms.Button();
+            this.durdur = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(401, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // baslat
+            // 
+            this.baslat.Location = new System.Drawing.Point(160, 187);
+            this.baslat.Name = "baslat";
+            this.baslat.Size = new System.Drawing.Size(75, 23);
+            this.baslat.TabIndex = 3;
+            this.baslat.Text = "Baslat";
+            this.baslat.UseVisualStyleBackColor = true;
+            this.baslat.Click += new System.EventHandler(this.baslat_Click);
+            // 
+            // durdur
+            // 
+            this.durdur.Location = new System.Drawing.Point(349, 187);
+            this.durdur.Name = "durdur";
+            this.durdur.Size = new System.Drawing.Size(75, 23);
+            this.durdur.TabIndex = 4;
+            this.durdur.Text = "Durdur";
+            this.durdur.UseVisualStyleBackColor = true;
+            this.durdur.Click += new System.EventHandler(this.durdur_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.durdur);
+            this.Controls.Add(this.baslat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button baslat;
+        private System.Windows.Forms.Button durdur;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ sukru1/sukru1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sukru1
+{
+    public partial class Form1 : Form
+    {
+        int saniye = 0;
+        int salise = 0;
+        int dakika = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string constring = "Data Source=MSI\\SUKRU;Initial Catalog=sukru;Integrated Security=True;";
+        SqlConnection connect = new SqlConnection(constring);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void baslat_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void durdur_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            try
+            {
+                if (connect.State == ConnectionState.Closed)
+                { 
+                    connect.Open(); 
+
+                }
+                string kayit = "insert into zamankayitlari(saniye,salise,dakika,tarih) values(@saniye,@salise,@dakika,@tarih)";
+                SqlCommand komut = new SqlCommand(kayit, connect);
+                komut.Parameters.AddWithValue("@saniye", label2.Text);
+                komut.Parameters.AddWithValue("@salise", label3.Text);
+                komut.Parameters.AddWithValue("@dakika", label1.Text);
+                DateTime bugun = DateTime.Now;
+                komut.Parameters.AddWithValue("@tarih", bugun.ToString());
+
+                komut.ExecuteNonQuery();
+                connect.Close();
+                MessageBox.Show("Kayıt Eklendi");
+
+
+
+
+            }
+            catch(Exception hata)
+            {
+                MessageBox.Show("Kayıt Yüklenirken Hata Geldi" + hata.Message);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            salise++;
+            label3.Text = salise.ToString();
+            if (salise == 60)
+            {
+                saniye++;
+                salise = 0;
+            }
+            label2.Text = saniye.ToString();
+            if (saniye == 60)
+            {
+                dakika++;
+                saniye = 0;
+            }
+            label1.Text = dakika.ToString();
+        }
+    }
+}
